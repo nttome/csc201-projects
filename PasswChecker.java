687245bf6f2280da5683f035cf883aae00691197
Nicole Tome <97120660+nttome@users.noreply.github.com>
2022-02-06 19:44:56 -0500

    Update PasswChecker.java

--- PasswChecker.java
+++ PasswChecker.java
@@ -5,7 +5,7 @@ August 20, 2021
 //This program checks submitted password against the 10 most commonly used passwords for safety. Prompts for re-entry if user input matches commonly used list. 
 
 import java.util.Scanner;
-public class Unit6Ex6
+public class PasswChecker
 {
 
         private static String[] common_pass = {"123456","123456789","qwerty","password","111111","12345678","abc123","1234567","password1","12345"};
